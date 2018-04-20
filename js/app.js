ec441a2f77065b54bb6ae144552524d8106e2b3a
Islam <islam.sayed8@gmail.com>
2018-04-20 17:38:20 +0200

    fix: Add update and render methods

--- js/app.js
+++ js/app.js
@@ -25,9 +25,19 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
+
     this.sprite = 'images/char-boy.png';
 };
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
 
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
