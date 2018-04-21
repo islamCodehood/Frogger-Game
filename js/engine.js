b6eebea873a7dc4139a0d332dc330ba29b4da4e6
Islam <islam.sayed8@gmail.com>
2018-04-21 11:12:33 +0200

    fix: Add more images to cach

--- js/engine.js
+++ js/engine.js
@@ -173,7 +173,12 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png',
+        'images/Gem Green.png'
     ]);
     Resources.onReady(init);
 
