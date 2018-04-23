dbd7709cda231bdfa23a9df78db8440affd5e8cd
Islam <islam.sayed8@gmail.com>
2018-04-23 11:08:04 +0200

    Refact: update code for speed , event listener

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 //Get lifeLost audio element
 const lifeLost = document.getElementById('life-lost');
-
+//Get win audio element
+const winAudio = document.getElementById('win-audio');
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = -100;
+    //x, and y coordinates for enemy
+    this.x = x;
+    this.y = y;
+    //speed for enemy 
+    this.speed = Math.floor((Math.random() * 200) + 100);
 };
 
 // Update the enemy's position, required method for game
@@ -18,42 +23,26 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    enemy1.x += 65 * dt;
-    if (enemy1.x > 505) {
-        enemy1.x = -100;
-    }
-    enemy2.x += 50 * dt;
-    if (enemy2.x > 505) {
-        enemy2.x = -100;
-    }
-    enemy3.x += 80 * dt;
-    if (enemy3.x > 505) {
-        enemy3.x = -100;
-    }
-    enemy4.x += 70 * dt;
-    if (enemy4.x > 505) {
-        enemy4.x = -100;
-    }
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    this.x = this.x + this.speed *dt;
     handleCollision();
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    enemy1.y = 62;
-    enemy2.y = 145;
-    enemy3.y = 145;
-    enemy4.y = 228;
+    if (this.x > 505) {
+        this.x = -100;
+    }
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {
+var Player = function(x, y) {
     this.sprite = 'images/char-boy.png';
-    this.x = 202;
-    this.y = 405;
+    //x, and y coordinates for player
+    this.x = x;
+    this.y = y;
 };
 Player.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
@@ -64,13 +53,10 @@ Player.prototype.update = function(dt) {
         this.y *= dt;
     }; 
     handleCollision();
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-
 };
 
 // Draw the player on the screen, required method for game
 Player.prototype.render = function() {
-
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -78,25 +64,28 @@ Player.prototype.render = function() {
 Player.prototype.handleInput = function(movement) {
     if ((movement == 'left') && (this.x > 0)) {
         this.x -= 101;
-        console.log(this.y);
-
     } else if ((movement == 'right') && (this.x < 404)) {
         this.x += 101;
-        console.log(this.y);
-
     } else if ((movement == 'up') && ((this.y > 0))) {
         this.y -= 83;
-        console.log(this.y);
         if (this.y == -10) {
+            //pause game audio and play win audio
+            audio.pause();
+            winAudio.play();
+            //pause event listener to prevent any action before restart the game
+            document.removeEventListener('keyup', keyUpHandle);
             setTimeout(function(){
+                //return player to starting position
                 player.x = 202;
                 player.y = 405;
-            }, 300);        
+                //replay game audio
+                audio.play();
+                //return event listener
+                document.addEventListener('keyup', keyUpHandle);
+            }, 3000);        
         }
     } else if ((movement == 'down') && ((this.y < 405))) { 
         this.y += 83;
-        console.log(this.y);
-
     }
     ctx.drawImage(Resources.get(this.sprite), this.x , this.y);
 }
@@ -104,20 +93,22 @@ Player.prototype.handleInput = function(movement) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var enemy1 = new Enemy();
-var enemy2 = new Enemy();
-var enemy3 = new Enemy();
-var enemy4 = new Enemy();
+var enemy1 = new Enemy(-50, 62);
+var enemy2 = new Enemy(0, 145);
+var enemy3 = new Enemy(-200, 145);
+var enemy4 = new Enemy(-100, 228);
 
 var allEnemies = [enemy1, enemy2, enemy3, enemy4];
 
-var player = new Player();
+var player = new Player(202, 405);
 
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', keyUpHandle);
+
+function keyUpHandle(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -126,8 +117,7 @@ document.addEventListener('keyup', function(e) {
     };  
 
     player.handleInput(allowedKeys[e.keyCode]);
-});
-
+}
 /*handleCollision function do the following:
 *check if the player and bug are on the same position.
 *If so 1. pause the game audio. 2. play lifeLost audio.
@@ -137,17 +127,30 @@ function handleCollision() {
     allEnemies.forEach(function(enemy) {
         setInterval(function() {
             if ((player.x - enemy.x < 70) && (player.x - enemy.x > - 15) && (player.y - enemy.y == 11)){
+                    //pause game audio and play life lost audio
                     audio.pause();
                     lifeLost.play();
+                    //return player to starting position
                     player.x = 202;
-                    player.y = 405;
+                    player.y = 405; 
+                    //pause event listener to prevent any action before restart the game
+                    document.removeEventListener('keyup', keyUpHandle);
                     setTimeout(function() {
+                        //replay game audio
                         audio.play(); 
-                    }, 3000);     
+                        //return event listener
+                        document.addEventListener('keyup', keyUpHandle);
+                    }, 2000);  
             }
         }, 0);
     });    
 }
+
+//control audio (on/off)
+/*function controlAudio() {
+    if ()
+}*/
+
 
 
 
