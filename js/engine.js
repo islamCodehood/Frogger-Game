d0a90d17c7ff19b0ce672ab38618ccbfc5055d29
Islam <islam.sayed8@gmail.com>
2018-04-23 13:05:19 +0200

    fix: Add images to Resources

--- js/engine.js
+++ js/engine.js
@@ -29,7 +29,6 @@ var Engine = (function(global) {
     canvas.width = 505;
     canvas.height = 606;
     controls.insertAdjacentElement('afterend', canvas);
-
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
      */
@@ -182,9 +181,11 @@ var Engine = (function(global) {
         'images/char-boy.png',
         'images/char-cat-girl.png',
         'images/char-horn-girl.png',
-        'images/char-pink-girl.png',
         'images/char-princess-girl.png',
-        'images/Gem Green.png'
+        'images/Gem Green.png',
+        'images/Heart.png',
+        `images/icons8-audio-40.png`,
+        'images/icons8-no-audio-40.png'
     ]);
     Resources.onReady(init);
 
