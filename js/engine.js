5a4cdea248f57e2373b9e1137ee53ec73fac342e
Islam <islam.sayed8@gmail.com>
2018-04-24 15:10:36 +0200

    fix: invoke update & render on gem

--- js/engine.js
+++ js/engine.js
@@ -103,6 +103,7 @@ var Engine = (function(global) {
         allHearts.forEach(function(heart) {
             heart.update();
         });
+        gem.update();
     }
 
     /* This function initially draws the "game level", it will then call
@@ -158,6 +159,7 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
+        gem.render();
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
